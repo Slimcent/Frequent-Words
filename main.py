111040025ae6a21d6bc094c5beea5e3b785bbb07
Achara Obinna Vincent <33299753+Slimcent@users.noreply.github.com>
2023-02-20 00:35:24 +0100

    frequent words

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def pattern_count(text, pattern):
+    count = 0
+    for i in range(len(Text) - len(pattern) + 1):
+        if text[i:i + len(pattern)] == pattern:
+            count = count + 1
+    return count
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def frequency_map(text, k):
+    freq = {}
+    n = len(Text)
+    for i in range(n - k + 1):
+        pattern = text[i:i + k]
+        freq[pattern] = pattern_count(Text, pattern)
+    return freq
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def frequent_words(text, k):
+    words = []
+    freq = frequency_map(Text, k)
+    m = max(freq.values())
+    for key in freq:
+        if freq[key] == m:
+            words.append(key)
+            words.sort()
+    return words
+
+
+Text = "ACGTTGCATGTCGCATGATGCATGAGAGCT"
+k = 4
+
+print(frequent_words(Text, k))
